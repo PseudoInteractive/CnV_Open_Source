da5c84969b8fe806558879b8f6c35d2e498feed6
David Wu <dwu@live.ca>
2020-09-23 09:39:15 -0700

    up

---
+++ COTG/Game/Attack.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COTG.Game
+{
+    public class Attack
+    {
+        int player { get; set; }
+        int target { get; set; }
+        public enum Type
+        {
+            assult,
+            senator,
+            se, 
+        }
+        public Type type { get; set; }
+        public bool fake { get; set; }
+    }
+}
