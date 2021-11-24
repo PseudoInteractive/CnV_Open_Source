09b07ddcc2ebcbcf2467ab93cb71040e3216b82b
David Wu <avatar@conquestandvirtue.com>
2021-11-23 23:36:22 -0800

    Move stuff around

--- COTG/GlobalUsing.cs
+++
@@ -1,9 +0,0 @@
-﻿global using System.Linq;
-global using System;
-global using COTG.Game;
-global using COTG.Views;
-global using COTG.Helpers;
-global using MessagePack;
-global using static COTG.Debug;
-global using System.Collections.Immutable;
-global using Nito.AsyncEx;
