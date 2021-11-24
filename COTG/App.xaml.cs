361afd87ec2bfc5c56a8877dcf50dbfabc73c1c4
David Wu <avatar@conquestandvirtue.com>
2021-11-23 19:58:17 -0800

    Test

--- COTG/App.xaml.cs
+++ COTG/App.xaml.cs
@@ -493,7 +493,6 @@ namespace COTG
 				}
 				//// Get the screen resolution (APIs available from 14393 onward).
 				//var resolution = new Size(view.ScreenWidthInRawPixels, view.ScreenHeightInRawPixels);
-
 				//// Calculate the screen size in effective pixels. 
 				//// Note the height of the Windows Taskbar is ignored here since the app will only be given the maxium available size.
 				//var scale = view.ResolutionScale == ResolutionScale.Invalid ? 1 : view.RawPixelsPerViewPixel;
