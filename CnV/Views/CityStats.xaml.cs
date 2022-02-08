2d0ffe43e2f23472469610193aa71e9564d8caca
David Wu <avatar@conquestandvirtue.com>
2022-02-08 01:35:47 -0800

    checkin

--- CnV/Views/CityStats.xaml.cs
+++ CnV/Views/CityStats.xaml.cs
@@ -18,7 +18,8 @@ using Windows.Foundation.Collections;
 using Microsoft.UI;
 using CommunityToolkit.WinUI.UI;
 using CommunityToolkit.WinUI.UI.Controls;
-using Expander = CommunityToolkit.WinUI.UI.Controls.Expander;
+
+//using Expander = CommunityToolkit.WinUI.UI.Controls.Expander;
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
 
@@ -49,14 +50,18 @@ namespace CnV
 			if(city.IsInvalid())
 				return;
 
+			var dummy = expResource.Children<TextBlock>(8);
+			dummy = expBuildQuerue.Children<TextBlock>(8);
+			dummy = expResource.Children<TextBlock>(8);
+
 			var resources = city.SampleResources();
+			var panels = expResource.Child<WrapPanel>().Children<StackPanel>();
 			for(var r = 0; r< CnV.Resources.idCount; r++)
 			{
-				var panel = expResource.FindChild<WrapPanel>();
-				var ch = panel.FindChildren().ElementAt(r).FindChildren();
-				
-				var txt = ch.ElementAt(0) as TextBlock;
-				var prod = ch.ElementAt(1) as TextBlock;
+				var ch = panels.ElementAt(r).Children<TextBlock>();
+				Assert(ch.Count==2);
+				var txt = ch.ElementAt(0);
+				var prod = ch.ElementAt(1);
 
 //				var txt = r switch { 0 => res0, 1 => res1, 2 => res2, _ => res3 };
 //				var prod = r switch { 0 => prod0, 1 => prod1, 2 => prod2, _ => prod3 };
@@ -80,7 +85,7 @@ namespace CnV
 			}
 		}
 
-		private void Expander_Expanding(Expander sender, ExpanderExpandingEventArgs args)
+		private void Expander_Expanding(Microsoft.UI.Xaml.Controls.Expander sender, ExpanderExpandingEventArgs args)
 		{
 			UpdateUI();
 		}
@@ -88,6 +93,13 @@ namespace CnV
 		private void scroll_SizeChanged(object sender, SizeChangedEventArgs e)
 		{
 			stackPanel.Width = e.NewSize.Width;
+		}
+
+		
+		private void Expander_Expanded(object sender, EventArgs e)
+		{
+			UpdateUI();
+
 		}
 	}
 }
